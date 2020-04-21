5b0f58b1a39a2356fa7c6ebf03752120be9a6ef3
Grace Tveit <grace.tveit@gmail.com>
2020-04-21 16:15:26 -0700

    Fixed using a function

--- juggling-async.js
+++ juggling-async.js
@@ -5,19 +5,20 @@ const bl = require('bl');
 let input = process.argv.splice(2);
 let responses = [];
 let callbackCount = 0;
-let index = 0;
 
-for (let i = 0; i < input.length; i++) {
-    http.get(input[i], (res) =>{
+function get (index) {
+    http.get(input[index], (res) =>{
         res.pipe(bl((err, data) => {
             if (err) throw err;
-            responses[callbackCount] = data.toString();
+            responses[index] = data.toString();
             callbackCount++;
-            if (callbackCount < input.length) {responses.forEach(data => {
+            if (callbackCount == 3) {responses.forEach(data => {
                 console.log(data);
             })}
         }))
-        index++;
     })
-
 }
+
+for (let i = 0; i < input.length; i++) {
+    get(i);
+};
