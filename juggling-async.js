b613e47e5919ddcd262989604e539a74add84e80
Grace Tveit <grace.tveit@gmail.com>
2020-04-21 15:56:34 -0700

    help

--- juggling-async.js
+++ juggling-async.js
@@ -2,19 +2,22 @@
 const http = require('http');
 const bl = require('bl');
 
-let input = process.argv;
+let input = process.argv.splice(2);
 let responses = [];
 let callbackCount = 0;
+let index = 0;
 
-for (let i = 2; i < input.length; i++) {
+for (let i = 0; i < input.length; i++) {
     http.get(input[i], (res) =>{
         res.pipe(bl((err, data) => {
             if (err) throw err;
             responses[callbackCount] = data.toString();
             callbackCount++;
-            if (callbackCount == 3) {responses.forEach(data => {
+            if (callbackCount < input.length) {responses.forEach(data => {
                 console.log(data);
             })}
         }))
+        index++;
     })
+
 }
